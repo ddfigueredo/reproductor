edb235adbad0eabdd00e2f1da472495fb148e1fd
demian con e <64447286+ddemiancone@users.noreply.github.com>
2020-08-02 08:18:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+	const playBtn = document.querySelector('.play')
+	const pauseBtn = document.querySelector('.pause')
+	const previousBtn = document.querySelector('.previous')
+	const nextBtn = document.querySelector('.next')
+	const firstRow = document.querySelector('.first-row')
+	const audio = document.querySelector('audio')
+
+	window.onclick = function(e) {
+		if (e.target = playBtn && audio.paused) {
+			playBtn.classList.toggle('hide')
+			pauseBtn.classList.toggle('hide')
+			audio.play()
+		}
+
+		else if (e.target = pauseBtn && audio.played) {
+			playBtn.classList.toggle('hide')
+			pauseBtn.classList.toggle('hide')
+			audio.pause()
+		}
+	}
+
+
+}
